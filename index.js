65841634f553f499720bd72b366ebbf46b0da068
Mark <markwilkins86@gmail.com>
2022-05-06 15:45:57 -0400

    commit tree 1d7012bd30c50f12496f75740c70a34984defa5d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: 'James',
+            email: 'jamesperters@timetoemail.com'
+        })
+      })  
+    .then(function(response) {
+        response.json()
+    })
+    .then(function(object) {
+        document.body.innerHTML = object('id')
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message 
+    })
+}
